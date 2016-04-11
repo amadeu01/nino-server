9209090052c2c56aa0431ac7036c1f4486a52f5f
amadeu01 <amadeu01@gmail.com>
2016-04-11 11:32:52 -0300

    Minor fixes

--- api/services/guardian.js
+++ api/services/guardian.js
@@ -4,101 +4,81 @@
 */
 
 var models = require('../models');
-var User = models.waterline.collections.user;
+var Users = models.waterline.collections.user;
 var Guardians = models.waterline.collections.guardian;
 var Roles = models.waterline.collections.role;
 var Devices = models.waterline.collections.device;
 var Credentials = models.waterline.collections.credential;
 
+//errors and validator's module
+var errors = require('../errors');
+var validator = require('validator');
+
 var guardiansServices = {
 	create: function(parameters) {
-		return User.create({
-			name: parameters.name,
-			surname: parameters.surname,
-			password: parameters.password,
-			email: parameters.email,
-			cel: parameters.cel,
-			confirmed: parameters.confirmed
-		}).then(function (user) {
-				return Roles.create({
-					privileges: parameters.privileges,
-					owner: user.id,
-					type: 'guardian'
-				}).then(function(role){
-					user.roles = [role];
-					return Devices.create({
-						arn: 'asfnancabprwuei1924830149324',
-						description: 'Here a mobile owned by a Guardian',
-						enable: true,
-						owner: user.id
-						}).then(function(device) {
-							user.devices = [device];
-							return Credentials.create({
-								owner: user.id,
-								devices: device.id
-							}).then(function(credential){
-								user.credentials = [credential];
-								credential.devices = [device];
-								role.save();
-								device.save();
-								credential.save();
-								return user.save();
-							}).catch(function(err) {
-								if (err) {
-									console.log(err, err.stack);
-								}
-							});
-					});
+		if (!validator.isEmail(parameters.user.email)) throw errors.invalidParameters('Invalid User email');
+
+		return Users.create({
+				name: parameters.user.name,
+				surname: parameters.user.surname,
+				password: parameters.user.password,
+				email: parameters.user.email,
+				cel: parameters.user.cel
+		})
+		.then(function(user) {
+			if (!user) throw errors.internalError('User - Creation Error');
+			return Roles.create({
+				type: 'guardian',
+				privileges: parameters.privileges,
+				owner: user.id
 			});
+		})
+		.then(function(role) {
+			if (!role) throw errors.internalError('Role - Creation Error');
+			return Guardians.create({
+				role: role.id,
+				school: parameters.schoolID
+			});
+		})
+		.then(function(guardian) {
+			if (!guardian) throw errors.internalError('Guardian - Creation Error');
+			return ({guardian: guardian.id});
 		});
 	},
 	delete: function(parameters) {
-		return User.destroy(parameters).then(function(users){
-			// console.log(users);
-			var userIds = users.map(function(user){return user.id;});
-			// console.log(userIds);
-			return Devices.destroy({owner: userIds}).then(function(devices){
-				// console.log(devices);
-				return Credentials.destroy({owner: userIds}).then(function(credentials) {
-					// console.log(credentials);
-					return Roles.destroy({owner: userIds}).then(function(roles) {
-						// console.log(roles);
-					}).catch(function(err) {
-						if (err) {
-							return console.log(err);
-						}
-					});
-				});
-			});
+		if (!parameters) throw errors.invalidParameters('Missing Parameter');
+		return Guardians.findOne(parameters)
+		.then(function(guardian) {
+			if (!guardian) throw errors.inexistentRegister('Guardian - Finding Error');
+			guardian.active = false;
+			guardian.save();
+			return Roles.findOne({id: guardian.role});
+		})
+		.then(function (role) {
+			role.active = false;
+			return role.save();
 		});
-
 	},
 	read: function(parameters) {
-		//Find and populate with information all user matched with the given parameters
-		return User.find(parameters).populate(['roles', 'credentials', 'devices']).then(function(users){
-			if (users.length === 0) {
-				throw Error('No user matched with the given parameters!');
-			}
-			return users;
-		}).catch(function(err) {
-			throw err;
+		if (!parameters) throw errors.invalidParameters('Missing Parameter');
+		parameters.active = true;
+		return Guardians.findOne(parameters)
+		.then(function (guardian) {
+			if (!guardian) return undefined;
+			return Roles.findOne({id: guardian.role}).populate('owner');
 		});
 	},
-	update: function(parameters, newParameters) {
-		return User.find(parameters).then(function(users){
-			// console.log(parameters);
-			if (users.length === 0 ) {
-				throw Error('NO user was found');
-			}
-			if (users.length >= 2) {
-				// console.log(users.pop());
-				throw Error('More than 1 user was found, it should be unique for changing.');
-			}
-			return User.update(parameters, newParameters).then(function(user){
-				// console.log(user);
-			}).catch(function(err) {
-				return console.log(err);
-			});
+	update: function(parameters, newParatemers, roleParameters) {
+		if (!parameters || !newParatemers || !roleParameters) throw errors.invalidParameters('Missing Parameter');
+		parameters.active = true;
+		return Guardians.update(parameters, newParatemers)
+		.then(function(guardian) {
+			if (!guardian) throw errors.inexistentRegister('Educator - Finding Error');
+			return Roles.findOne({id: guardian[0].role});
+		})
+		.then(function(role) {
+			if (!role) throw errors.inexistentRegister('Role - Finding Error');
+			return Roles.update({id: role.id}, roleParameters);
 		});
 	}
 };
