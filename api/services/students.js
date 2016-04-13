7b4aa9c3da53182f7288333826f7c55795cc1346
amadeu01 <amadeu01@gmail.com>
2016-04-13 08:52:18 -0300

    Minor changes

--- api/services/students.js
+++ api/services/students.js
@@ -4,6 +4,14 @@
 */
 
 var models = require('../models');
+var Users = models.waterline.collections.user;
+var Roles = models.waterline.collections.role;
+var Devices = models.waterline.collections.device;
+var Credentials = models.waterline.collections.credential;
+
+//errors and validator's module
+var errors = require('../business/errors');
+var validator = require('validator');
 
 var studentsServices = {
 	create: function(params, done) {
