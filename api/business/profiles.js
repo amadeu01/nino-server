cadef26f368277ca6d4bf6388095f3fe77c5f14c
cmillani <carloseduardomillani@gmail.com>
2016-07-14 11:56:01 -0300

    Profile pic form better implemented...using mechanisms, routes and business better

--- api/business/profiles.js
+++ api/business/profiles.js
@@ -5,22 +5,13 @@ var response = require('../mechanisms/response.js') ;
 var awss3 = require('../mechanisms/AWSS3.js');
 var activitiesDAO = require('../persistence/profiles.js');
 var profiles = {};
-var multiparty = require('multiparty');
 
-profiles.uploadProfilePicture = function(profileID, rawToken, device, req) {
+profiles.uploadProfilePicture = function(profileID, rawToken, device, part) {
 	return new Promise(function(resolve, reject) {
-		var gotImage = false;
 		credentialDAO.read(rawToken)
 		.then(function(credential){
-			if ((credential.device !== device)) throw (errors.invalidParameters("device"));
+			if ((credential.device !== device)) reject(errors.invalidParameters("device"));
 			else {
-				console.log("Starting form");
-				var form = new multiparty.Form();
-				form.on('error', function(err) {
-					reject(errors.internalError(err));
-				});
-				form.on('part', function(part) {
-					gotImage = true;
 					awss3.uploadProfilePic(part, "pp_" + profileID + ".png", part.byteCount)
 					.then(function(success) {
 						resolve( new response(200, success, null));
@@ -28,12 +19,6 @@ profiles.uploadProfilePicture = function(profileID, rawToken, device, req) {
 						reject(errors.internalError(err));
 					});
 				});
-				form.on('close', function() {
-					if (!gotImage) {
-						reject(errors.missingParameters("picture"));
-					}
-				});
-				form.parse(req);
 			}
 		})
 		.catch(function(err) {
--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var errors = require('../mechanisms/error');
 var profileBO = require('../business/profiles.js');
 var validator = require('validator');
+var multiparty = require('multiparty');
 
 var numberValidate = function(req, res, next, id) {
 	if (!isNaN(id)) {
@@ -19,18 +20,34 @@ router.post("/me/picture", function(req, res, next) {
 		if (req.token === undefined ) missingParameters.push("token");
 		if (req.rawToken === undefined) missingParameters.push("rawToken");
 		if (req.device === undefined) missingParameters.push("device");
-		if (req.body.picture === undefined) missingParameters.push("picture");
 		if (missingParameters.length > 0) reject(errors.missingParameters(missingParameters));
 		else {
-			profileBO.uploadProfilePicture(token.profile, rawToken, req.device, req)
-			.then(function(result) {
-				res.status(result.code).json(result.json);
-				resolve(result);
-			})
-			.catch(function(err) {
-				res.status(err.code).json(err.json);
-				resolve(err);
+			var form = new multiparty.Form();
+			form.on('error', function(err) {
+				reject(errors.internalError(err));
 			});
+			form.on('part', function(part) {
+				if (part.name !== "picture") {
+					part.resume();
+					return;
+				}
+				gotImage = true;
+				profileBO.uploadProfilePicture(token.profile, rawToken, req.device, part)
+				.then(function(result) {
+					res.status(result.code).json(result.json);
+					resolve(result);
+				})
+				.catch(function(err) {
+					res.status(err.code).json(err.json);
+					resolve(err);
+				});
+			});
+			form.on('close', function() {
+				if (!gotImage) {
+					reject(errors.missingParameters("picture"));
+				}
+			});
+			form.parse(req);
 		}
 	}).catch(function(err){
 		res.status(err.code).json(err.json);
