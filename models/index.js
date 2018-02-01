b59a72891bbe03f16e3d0b5878d92b58b20288d8
amadeu <amadeu01@gmail.com>
2018-02-01 15:12:34 -0200

    Fixed models config to more generic Database

--- models/index.js
+++ models/index.js
@@ -2,49 +2,12 @@
 * Amadeu Cavalcante
 * Module for models warterline
 */
-//var mysqlAdapter = require('sails-mysql');
-//var myDiskAdapter = require('sails-disk');
-//var myPsql = require('sails-postgresql');
-//var Waterline = require('waterline');
-//var orm = new Waterline();
-//var config = {
-  // setup adapters for each type of database
-  //I do not know which db will be used, so for test's sake I used more than one.
-  //adapters: {
-    //'default': mysqlAdapter,
-    //'disk': myDiskAdapter,
-	//'psql': myPsql
-  //},
-  // setup all type of connections you could have
-  //connections: {
-    // NinoDB
-    // default: {
-    //   				adapter: 'default',
-    //   				host: 'ninodbinstance.cwtfdu1insah.sa-east-1.rds.amazonaws.com',
-    //   				user: 'ninodbuser',
-    //   				// password: 'CfJ-Cur-69S-pNz',
-    //   				database: 'NinoDB'
-    //   			},
-    //using sails-disk method
-	//
-	//default: {
-	 // adapter: 'psql',
-	 // database: 'ubuntu',
-	 // host: 'localhost',
-     // user: 'nino',
-	 // password: 'nino'
-	//}
-    //default: {
-    //    adapter: 'disk'
-    //}
-  //},
-//};
 
 var config = {
-	user: 'nino',
-	password: 'chupaagendakids',
-	database: 'nino',
-	host: 'ninoapp-production.cwtfdu1insah.sa-east-1.rds.amazonaws.com',
+	user: 'process.env.DATABASE_USER',
+	password: 'process.env.DATABASE_PASSWORD',
+	database: 'process.env.DATABASE_NAME',
+	host: 'process.env.DATABASE_URL',
 	port: 5432,
 	max: 10,
 	idleTimeoutMillis: 30000,
